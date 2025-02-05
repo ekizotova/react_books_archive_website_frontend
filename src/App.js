edf563b5a2d507bb13344642f4fa7d4c1c515297
Ekaterina <ekaterina.izotova.work@gmail.com>
2025-02-05 22:39:47 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './components/MainPage';
+import AuthorsList from './components/AuthorsList';
+import BooksList from './components/BooksList';
+import BookstoresList from './components/BookstoresList';
+import PublihsersList from './components/PublihsersList';
+import PublishedBy from './components/PublishedBy';
+import ListInStock from './components/ListInStock';
+import WrittenBooks from './components/WrittenBooks';
+import Search from './components/Search';
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<MainPage />} />
+          <Route path="/authors" element={<AuthorsList />} />
+          <Route path="/books" element={<BooksList />} />
+          <Route path="/bookstores" element={<BookstoresList />} />
+          <Route path="/publishers" element={<PublihsersList />} />
+		  <Route path="/books/publishedBy/:idBook" element={<PublishedBy />} />
+		  <Route path="/books/inStock/:idBook" element={<ListInStock />} />
+		  <Route path="/books/search" element={<Search />} />
+		  <Route path="/authors/writtenBooks/:idAuthor" element={<WrittenBooks />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
