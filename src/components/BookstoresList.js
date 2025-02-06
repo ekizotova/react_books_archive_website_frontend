49864e5a22e113fdfa7b411b420e87b6f0b606f4
Ekaterina <ekaterina.izotova.work@gmail.com>
2025-02-06 23:41:40 +0100

    Create BookstoresList.js

---
+++ src/components/BookstoresList.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { GetAllBookstores } from './Requests';
+
+function BookstoresList() {
+ 
+  const [bookstores, setBookstores] = useState([]);
+
+    useEffect(() => {
+        const fetchBookstores = async () => {
+            try {
+                const fetchedBookstores = await GetAllBookstores();
+                setBookstores(fetchedBookstores);
+            } catch (error) {
+                console.error("Error fetching bookstores:", error);
+          
+            }
+        };
+
+        fetchBookstores();
+    }, []);
+
+  return (
+    <div>
+      <h1>List of bookstores</h1>
+				<table>
+                    <thead>
+                        <tr>
+                            <th> </th>
+                            <th>name</th>
+                            <th>location</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {bookstores.map(bookstore => (
+                            <tr key={bookstore.idBookstore}>{/* No whitespace before or after this line */}
+                                <td> {bookstore.idBookstore} </td>
+								<td> {bookstore.storeName} </td>
+                                <td> {bookstore.location} </td>
+                                
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+    </div>
+  );
+}
+
+export default BookstoresList;
